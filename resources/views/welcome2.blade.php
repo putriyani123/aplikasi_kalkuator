8990f01fd9aa11d20cd232239f53ab7d7e99ab65
codeandini <smkiu.004@gmail.com>
2026-04-20 10:56:09 +0700

    menambah welcome

---
+++ resources/views/welcome2.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>selamat datang</title>
+</head>
+<body>
+    <p>selamat datang di sini</p>
+</body>
+</html>
